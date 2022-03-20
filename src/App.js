54b64e8778d3b3057a31489f346a81c27d006533
Adriel <adriel.fariasan@gmail.com>
2022-03-20 16:30:55 -0300

    Adiciona funcao incrementarContador

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [contador, setContador] = useState(0);
+  const incrementarContador = () => setContador(contador + 1);
   return <h1>React hooks</h1>;
 }
 
