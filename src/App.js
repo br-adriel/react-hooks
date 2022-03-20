f7082944968556203623bba99ad78b3c8ea1362d
Adriel <adriel.fariasan@gmail.com>
2022-03-20 16:32:01 -0300

    Adiciona mostrador para contador

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   const [contador, setContador] = useState(0);
   const incrementarContador = () => setContador(contador + 1);
-  return <h1>React hooks</h1>;
+  return (
+    <>
+      <h1>React hooks</h1>
+      <h2>Contador: {contador}</h2>
+    </>
+  );
 }
 
 export default App;
